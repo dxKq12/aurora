68d595e59c63ef8fad4097928358c4678a88012b
dxKq12 <dxKq12@gmail.com>
2024-06-12 15:11:38 +0900

    Update memo.cpp

--- misc/memo.cpp
+++ misc/memo.cpp
@@ -1,3 +1,20 @@
-/*
-弾幕めーかー♡
-*/
+//弾幕メーカー(?)
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <string>
+
+std::string getRandomString(const std::vector<std::string>& strings) {
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    int numStrings = static_cast<int>(strings.size());
+    int randomIndex = std::rand() % numStrings;
+    return strings[randomIndex];
+}
+
+int main() {
+    std::vector<std::string> strings = {"r=\cos{\sqrt{\theta}}","sample1","sample2","sample3","sample4","sample5"};
+    std::cout << getRandomString(strings) << std::endl;
+
+    return 0;
+}
